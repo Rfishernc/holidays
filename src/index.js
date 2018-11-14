4bd00397c6abaa8abc8787a633abdfd2f654dde9
RichFisher <rich.fishernc@gmail.com>
2018-11-13 21:23:07 -0600

    hidey stuff

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import auth from './comp/auth/auth';
 import 'bootstrap';
 import './index.scss';
 import apiKeys from '../db/apiKeys.json';
+import authHelpers from './helpers/authHelpers';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   navBar.createNavBar();
+  authHelpers.checkLogInStatus();
   auth.logInButton();
 };
 
