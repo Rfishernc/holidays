10d5a3fbec08d9254f965a44baf31d55de493889
RichFisher <rich.fishernc@gmail.com>
2018-11-13 19:59:16 -0600

    authenticatin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import firebase from 'firebase/app';
 import navBar from './comp/navbar/navbar';
+import auth from './comp/auth/auth';
 import 'bootstrap';
 import './index.scss';
+import apiKeys from '../db/apiKeys.json';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   navBar.createNavBar();
+  auth.logInButton();
 };
 
 init();
